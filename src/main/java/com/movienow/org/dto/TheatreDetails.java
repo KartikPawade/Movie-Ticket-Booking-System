4a07b4924174fb9178cf35d6dae0cb365aef1463
kartik <kartikpawade25@gmail.com>
2024-02-21 01:15:50 +0530

    refactored code

---
+++ src/main/java/com/movienow/org/dto/TheatreDetails.java
@@ -0,0 +1,16 @@
+package com.movienow.org.dto;
+
+public  interface TheatreDetails {
+
+    Long getId();
+
+    String getName();
+
+    Long getAddressId();
+
+    String getArea();
+
+    Long getCityId();
+
+    String getCity();
+}
